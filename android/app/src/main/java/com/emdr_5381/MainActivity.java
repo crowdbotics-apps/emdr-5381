bf685d44fdfaed2efe1295423e07aef2f4f921e0
Crowdbotics <team@crowdbotics.com>
2019-06-30 04:17:22 +0000

    initial commit

---
+++ android/app/src/main/java/com/emdr_5381/MainActivity.java
@@ -0,0 +1,15 @@
+package com.emdr_5381;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "emdr_5381";
+    }
+}
